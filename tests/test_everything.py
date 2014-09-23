6a5ff58759ba53d2125e9c504ebdbd63a025e7e4
Finn Aarup Nielsen <faan@dtu.dk>
2014-09-23 17:56:29 +0200

    Initial version with a few tests.

---
+++ tests/test_everything.py
@@ -0,0 +1,16 @@
+
+from everything import *
+
+
+def test_collections():
+    assert Counter([1, 2, 2]) == {1: 1, 2: 2}
+
+
+def test_itertools():
+    assert type(zip_longest((1,), (1, 2))) == list
+    assert zip_longest((1,), (1, 2)) == [(1, 1), (None, 2)]
+
+
+def test_nltk():
+    if 'nltk' in sys.modules:
+        assert bigrams([1, 2, 3]) == [(1, 2), (2, 3)]
