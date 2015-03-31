f8524d78ccb101cde87074110d4735e5033ccbca
Finn Aarup Nielsen <faan@dtu.dk>
2015-03-31 14:43:12 +0200

    Fixed conditional testing. Extended with more tests.

--- test_everything.py
+++ test_everything.py
@@ -1,6 +1,10 @@
+"""Testing everything.py module."""
+
+
+import imp
 
 from everything import *
-import imp
+
 
 def test_collections():
     assert 'collections' in globals()
@@ -12,32 +16,62 @@ def test_itertools():
     assert list(zip_longest((1,), (1, 2))) == [(1, 1), (None, 2)]
 
 
+def test_json():
+    assert 'json' in globals()
+    assert json.loads(json.dumps({'a': [2, 3]})) == {'a': [2, 3]}
+
+
 def test_networkx():
     try:
         imp.find_module('networkx')
-        assert 'nx' in globals()
-    except:
+    except ImportError: 
         # networkx not available on platform
         pass
+    else:
+        assert 'nx' in globals()
 
 
 def test_nltk():
-     try:
+    try:
         imp.find_module('nltk')
+    except ImportError:
+        # nltk not available on platform
+        pass
+    else:
         assert 'nltk' in globals()
         assert word_tokenize('Hello world') == ['Hello', 'world']
         assert bigrams([1, 2, 3]) == [(1, 2), (2, 3)]
-     except:
-         # nltk not available on platform
-         pass
+
+
+def test_numpy():
+    try:
+        imp.find_module('numpy')
+    except ImportError:
+        pass
+    else:
+        assert 'np' in globals()
+        assert mean([1, 3]) == 2.0
+
+
+def test_pandas():
+    try:
+        imp.find_module('pandas')
+    except ImportError: 
+        pass
+    else:
+        assert 'pd' in globals()
+        assert DataFrame([[1, 2], [3, 4]])
+        assert DataFrame([[1, 2], [3, 4]]).ix[0, 0] == 1
+        assert Series([1, 2]).ix[0] == 1
 
 
 def test_scipy():
-     try:
+    try:
         imp.find_module('scipy')
+    except:
+        # scipy not available on platform
+        pass
+    else:
         assert 'scipy' in globals()
         assert 'stats' in globals()
         assert stats.norm(0, 1)
-     except:
-         # scipy not available on platform
-         pass
